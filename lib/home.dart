5502abbfe3c6302d04492202c8c06e0de034b251
theXtroyer1221 <jaddou2005@gmail.com>
2022-02-22 15:30:25 +0100

    Added details page for every post

--- lib/home.dart
+++ lib/home.dart
@@ -1,11 +1,24 @@
 // ignore_for_file: avoid_print
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:like_button/like_button.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:pinch_zoom_image_last/pinch_zoom_image_last.dart';
 
 FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+class Post {
+  final String id;
+  final String title;
+  final String text;
+  final String image;
+  final int price;
+  final Timestamp date;
+
+  const Post(this.id, this.title, this.text, this.image, this.price, this.date);
+}
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -39,57 +52,113 @@ class _HomePageState extends State<HomePage> {
               return ListView.builder(
                   itemCount: data.size,
                   itemBuilder: (context, index) {
+                    Post post = Post(
+                        data.docs[index].id,
+                        data.docs[index]["title"],
+                        data.docs[index]["text"],
+                        data.docs[index]["image"],
+                        data.docs[index]["price"],
+                        data.docs[index]["date"]);
                     return Container(
                       padding: const EdgeInsets.symmetric(vertical: 10),
                       child: Card(
                         margin: const EdgeInsets.all(3),
-                        child: Column(
-                          children: [
-                            PinchZoomImage(
-                              zoomedBackgroundColor:
-                                  const Color.fromRGBO(240, 240, 240, 1.0),
-                              image: Image.network(
-                                data.docs[index]["image"].toString(),
-                                height: 150,
-                                width: double.infinity,
-                                fit: BoxFit.cover,
+                        child: InkWell(
+                          onTap: () => {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const ImagePreview(),
+                                  settings: RouteSettings(arguments: post)),
+                            )
+                          },
+                          child: Column(
+                            children: [
+                              PinchZoomImage(
+                                zoomedBackgroundColor:
+                                    const Color.fromRGBO(240, 240, 240, 1.0),
+                                image: Image.network(
+                                  post.image,
+                                  height: 150,
+                                  width: double.infinity,
+                                  fit: BoxFit.cover,
+                                ),
                               ),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.fromLTRB(0, 0, 20, 0),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Expanded(
-                                    child: ListTile(
-                                      title: Text(
-                                          data.docs[index]["title"].toString()),
-                                      subtitle: Text(
-                                          data.docs[index]["text"].toString()),
+                              Container(
+                                margin: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Expanded(
+                                      child: ListTile(
+                                        title: Text(post.title),
+                                        subtitle: Text(post.text),
+                                      ),
                                     ),
-                                  ),
-                                  LikeButton(
-                                    likeCount: data.docs[index]["price"],
-                                    onTap: (bool isLiked) async {
-                                      var likes = data.docs[index]["price"];
-                                      FirebaseFirestore.instance
-                                          .collection("cards")
-                                          .doc(data.docs[index].id)
-                                          .update({"price": likes}).then(
-                                              (value) => print(likes));
+                                    LikeButton(
+                                      likeCount: post.price,
+                                      onTap: (bool isLiked) async {
+                                        var likes = post.price;
+                                        FirebaseFirestore.instance
+                                            .collection("cards")
+                                            .doc(post.id)
+                                            .update({"price": likes}).then(
+                                                (value) => print(likes));
 
-                                      return !isLiked;
-                                    },
-                                  )
-                                ],
+                                        return !isLiked;
+                                      },
+                                    )
+                                  ],
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
                     );
                   });
             }));
+  }
+}
+
+class ImagePreview extends StatelessWidget {
+  const ImagePreview({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final post = ModalRoute.of(context)!.settings.arguments as Post;
+    return Scaffold(
+      appBar: AppBar(title: Text(post.title)),
+      body: Container(
+        alignment: Alignment.center,
+        width: double.infinity,
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Image.network(
+              post.image,
+              height: 300,
+              width: double.infinity,
+              fit: BoxFit.fitHeight,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Text(
+              post.title,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Text(
+              post.text,
+              style: const TextStyle(),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
